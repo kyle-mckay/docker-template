285be250626eefd2b905f956c1b8db6994a1b1bc
Kyle <83886427+kyle-mckay@users.noreply.github.com>
2025-11-30 16:47:26 -0700

    Create companion script

---
+++ dc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# companion script for doing mass compose actions
+# Usage:
+# bash dc.sh up
+# OR allow script execution: `chmod +x dc.sh`
+# dc.sh up
+
+set -euo pipefail
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 {up|down|pull}"
+    exit 1
+fi
+
+cmd="$1"
+
+case "$cmd" in
+    up|down|pull)
+        ;;
+    *)
+        echo "Invalid command: $cmd"
+        echo "Usage: $0 {up|down|pull}"
+        exit 1
+        ;;
+esac
+
+for d in */; do
+    cd $d || continue
+	
+	if [[ ! -f '.skip' ]]; then
+		echo "=== Processing: $d ==="
+		if [[ "$cmd" == "up" ]]; then
+			docker compose up -d
+		elif [[ "$cmd" == "down" ]]; then
+			docker compose down
+		elif [[ "$cmd" == "pull" ]]; then
+			docker compose pull
+		fi
+	else
+		echo "=== Skipping: $d ==="
+	fi
+    cd ..
+done
